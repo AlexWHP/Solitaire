f85af60849bb6d44456148b1055766fe5eaa0407
AlexWHPhillips <87848579+AlexWHPhillips@users.noreply.github.com>
2022-12-26 17:10:08 +1300

    Game population
    Seperated into relevant files and added basic functionality to game population

---
+++ CardDeck.py
@@ -0,0 +1,63 @@
+from enum import Enum
+import random
+
+random.seed(1)
+
+class Colour(Enum):
+    RED = 1
+    BLACK = 2
+class Suit(Enum):
+    CLUBS = 1
+    SPADES = 2
+    DIAMONDS = 3
+    HEARTS = 4
+
+class Card:
+    def __init__(self, colour, suit, value) -> None:
+        self.colour = colour
+        self.suit = suit
+        self.value = value
+        self.hidden = False
+        self.pile = None
+    def changePile(self, new_pile) -> None:
+        """  Called if a card is succesfully changed from one pile to another """
+        self.pile = new_pile
+    def __str__(self) -> str:
+        if self.hidden == False:
+            if self.value <= 10:
+                card_val = str(self.value)
+            elif self.value == 11:
+                card_val = "J"
+            elif self.value == 12:
+                card_val = "Q"
+            elif self.value == 13:
+                card_val = "K"
+            return self.colour.name + " " + self.suit.name[0] + " " + card_val
+        else:
+            return "Card is hidden"
+
+class Deck:
+    def __init__(self) -> None:
+        deck = []
+        suits = [Suit.CLUBS, Suit.SPADES, Suit.DIAMONDS, Suit.HEARTS]
+        for sui in suits:
+            if sui in [Suit.CLUBS, Suit.SPADES]:
+                col = Colour.BLACK
+            else:
+                col = Colour.RED
+            for i in range(1, 14):
+                deck.append(Card(col, sui, i))
+        self.deck = deck
+
+    def getCard(self, index) -> Card:
+        return self.deck[index]
+
+    def __str__(self) -> str:
+        deck_str = ""
+        for card in self.deck:
+            deck_str += str(card) + "\n"
+        return deck_str
+
+    def shuffle(self):
+        """ Randomizes the order of the contained card classes """
+        random.shuffle(self.deck)
---
+++ Piles.py
@@ -0,0 +1,60 @@
+class Piles:
+    def __init__(self) -> None:
+        self.stack = []
+    def resetStack(self):
+        self.stack = []
+    def removeTopCard(self):
+        self.stack.pop()
+    def validMove(self):
+        """ Determines if a card is able to be added on top of another from the pile """
+        raise NotImplementedError
+    def addCard(self, card):
+        """ Checks if a card is able to be added on top of the pile """
+        raise NotImplementedError
+    def removeCard(self):
+        """ Checks if a card can be removed from the pile """
+        raise NotImplementedError
+    def moveCards(self):
+        """   """
+        raise NotImplementedError
+
+class Foundation(Piles):
+    """ Initially empty but aimed to be filled with a single ordered suit """
+    def validMove(self, card):
+        """ Valid if an ace is moved onto an empty foundation or is the same suit and one value higher than the current top card """
+        if len(self.stack) > 0:
+            return card.value == 1
+        return card.suit != self.stack[-1].suit and self.stack[-1].value == card.value + 1
+    def addCards(self, cards):
+        if len(cards) == 1:
+            return self.validMove(cards[0])
+        return False
+
+class Tableau(Piles):
+    """ Workspace to help transfer cards to the Tableau """
+    def validMove(self, add_card, top_card):
+        # The tableau is empty meaning only a king can be added
+        if top_card == None:
+            return add_card.value == 13
+        # The tableau contains a card of opposite value that is one less than the card to add
+        return add_card.colour != top_card.colour and add_card.value == top_card.value - 1
+
+    def addCards(self, cards):
+        """ Appends the cards to the tableau if it's a valid move """
+        #if self.validMove(cards[0], self.stack[-1]):
+        self.stack += cards
+        return True
+        #return False
+
+    def moveCards(self, destination, index):
+        remaining = self.stack[:index]
+        to_remove = self.stack[index:]
+        # Alters the tableau if the destination is valid
+        if destination.addCards(to_remove):
+            self.stack = remaining
+    
+class Stock(Piles):
+    """ Rotating deck of cards to be pulled into the Foundations or Tableau"""
+    def rotate(self):
+        self.stack.insert(0, self.stack.pop())
+    
